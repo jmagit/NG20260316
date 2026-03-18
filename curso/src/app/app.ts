c6dd7ad10fa67dbae338db355ca4ecd1fe3f0081
Javier <35864946+jmagit@users.noreply.github.com>
2026-03-18 10:02:46 +0100

    solucion notoficaciones

--- curso/src/app/app.ts
+++ curso/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer, Header } from "./main";
+import { Footer, Header, NotificationModal, Notification } from "./main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header],
+  imports: [RouterOutlet, Footer, Header, NotificationModal, Notification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- curso/src/app/ejemplos/demos/demos.ts
+++ curso/src/app/ejemplos/demos/demos.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { LoggerService } from '../../../lib/my-library';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { LoggerService } from '../../../lib/my-library';
+import { Unsubscribable } from 'rxjs';
+import { NotificationService, NotificationType } from '../../common-services';
 
 @Component({
   selector: 'app-demos',
@@ -8,11 +10,29 @@ import { LoggerService } from '../../../lib/my-library';
   styleUrl: './demos.css',
   // providers: [LoggerService]
 })
-export class Demos {
-  constructor(private out: LoggerService) {
-    // out.error('Esto es una demo de error')
-    // out.warn('Esto es una demo de warn')
-    // out.info('Esto es una demo de info')
-    // out.log('Esto es una demo de log')
+export class Demos implements OnInit, OnDestroy {
+  private suscriptor: Unsubscribable | undefined;
+
+  constructor(public vm: NotificationService) { }
+
+  ngOnInit(): void {
+    this.suscriptor = this.vm.Notificacion.subscribe(n => {
+      if (n.Type !== NotificationType.error) { return; }
+      // window.alert(`Suscripción: ${n.Message}`);
+      // this.vm.remove(this.vm.Listado().length - 1);
+    });
   }
+
+  ngOnDestroy(): void {
+    if (this.suscriptor) {
+      this.suscriptor.unsubscribe();
+    }
+  }
+
+  // constructor(private out: LoggerService) {
+  //   out.error('Esto es una demo de error')
+  //   out.warn('Esto es una demo de warn')
+  //   out.info('Esto es una demo de info')
+  //   out.log('Esto es una demo de log')
+  // }
 }
